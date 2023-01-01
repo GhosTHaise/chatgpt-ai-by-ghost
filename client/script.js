c49207fcdd639a5c08f20bd1f37d755eb015b13e
GhosTHaise <Ghostrex2@gmail.com>
2023-01-01 13:42:43 +0300

    Create typing effect function using interval

---
+++ client/script.js
@@ -0,0 +1,44 @@
+import bot from "./assets/bot.svg"
+import user from "./assets/user.svg"
+
+const from = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
+
+let loadInterval;
+
+function loader(element){
+    element.textContent = "";
+    loadInterval = setInterval(()=>{
+        element.textContent += ".";
+        if(element.textContent === "...."){
+            element.textContent = ""
+        }
+    },300)
+}
+
+function typeText(element,text) {
+    let index = 0 ;
+    let interval =  setInterval(()=>{
+        if(index < text.length){
+            element.innerHTML += text.charAt(index);
+            index++;
+        }else{
+            clearInterval(interval);
+        }
+    },200)                                                                                                                           
+}
+
+function generateUniqueId(){
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi,value,uniqueId){
+    //template string to have possibility to insert variable
+    return(`
+        
+    `);
+}
